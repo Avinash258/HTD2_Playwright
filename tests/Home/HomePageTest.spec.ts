78bebf98f3fd58049525a9ae92c3e01429f880ba
Gona Vaishnavi <gona.vaishnavi@newvision-software.com>
2026-04-20 15:06:10 +0530

    all test cases from TC_46 to TC_50 are added

--- tests/Home/HomePageTest.spec.ts
+++ tests/Home/HomePageTest.spec.ts
@@ -11,6 +11,33 @@ test('TC_46 Verify scroll up button visible after scrolling down ',async ({appAc
     await appAction.home.openHomePage();
     await appAction.home.scrollToBottom();   
     await appAction.home.clickScrollUpButton();
+    await appAction.home.homePage.page.waitForTimeout(2000);
     const isAtTop =await appAction.home.verifyPageAtTop();
     await expect(isAtTop).toBeTruthy();
+});
+
+ test('TC_48 Verify subscription form in footer ',async ({appAction}) =>{
+    await appAction.home.openHomePage();
+    await appAction.home.scrollToBottom();   
+    const result =await appAction.home.verifySubscriptionSectionVisible();
+    await expect(result.heading).toBeTruthy();
+     await expect(result.email).toBeTruthy();
+      await expect(result.button).toBeTruthy();
+ });
+
+test('TC_49 Verify subscription with valid email ',async ({appAction}) =>{
+ await appAction.home.openHomePage();
+    await appAction.home.scrollToBottom(); 
+    await appAction.home.subscribeWithEmail('gona.vaishnavi@newvision-software.com');
+    const isSuccess = await appAction.home.isSubscriptionSuccessVisible();
+    await expect(isSuccess).toBeTruthy();
+});
+
+test('TC_50 Verify brands on products page ',async ({appAction}) =>{
+await appAction.home.openHomePage();
+await appAction.home.navigateToProducts();
+const isVisible =await appAction.home.isBrandSectionVisible();
+await expect(isVisible).toBeTruthy();
+const brands =await appAction.home.getBrandsList();
+await expect(brands.length).toBeGreaterThan(0);
 });
