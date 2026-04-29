6d2353775b460947a4a8da0f9d4b5de68727cdeb
Nilesh pal <Nilesh.pal@newvision-software.com>
2026-04-29 10:23:27 +0530

    normal changes

--- src/fixture/Nilesh.fixture.ts
+++ src/fixture/Nilesh.fixture.ts
@@ -1,22 +1,26 @@
 import { test as base, expect } from "@playwright/test";
 import testcase from "../testdata/testcase.json";
 import { TestCasesActions } from "../action/testcaseaction";
+import { ParaAction } from "../action/ParaRegAction";
+import para from "../testdata/para.json";
 
 type AppActions = {
   testCases: TestCasesActions;
+  paraAction: ParaAction;
 };
 type Fixtures = {
   gotoBaseUrl: void;
   appAction: AppActions;
 };
 export const test = base.extend<Fixtures>({
-  gotoBaseUrl: async ({ page }, use) => {
-    await page.goto(testcase.baseUrl);
+  gotoBaseUrl: [async ({ page }, use) => {
+    await page.goto(para.baseUrl);
     await use();
-  },
+  }, { auto: true }],
   appAction: async ({ page }, use) => {
     const appAction: AppActions = {
       testCases: new TestCasesActions(page),
+      paraAction: new ParaAction(page),
     };
     await use(appAction);
   },
