57473d00ab42d10baed784ddeeef81e8ba5c4933
LALIT1804-NEWVISION <lalit.gupta@newvision-software.com>
2026-04-29 10:49:01 +0530

    Initial commit2

--- src/action/homeaction.ts
+++ src/action/homeaction.ts
@@ -1,14 +1,66 @@
-import { HomePage } from "../page/homepage";//register
-import {Locator,Page} from "@playwright/test";
-export class HomeAction{
-   
-    
-    constructor (private readonly homePage: HomePage) {
-        
+import { expect, Page } from '@playwright/test';
+import { HomePage } from '../../src/page/homepage';
+
+export class HomeAction {
+
+    readonly homePage: HomePage;
+    readonly page: Page;
+
+    constructor(homePage: HomePage) {
+        this.homePage = homePage;
+        this.page = homePage.page;
     }
-    async clickOnSign(){
-        return  this.homePage.SignUp.click()
-        
+
+    async verifyHomePageLoaded() {
+        await expect(this.page).toHaveURL('/');
+        await expect(this.homePage.logo).toBeVisible();
+        await expect(this.homePage.navbar).toBeVisible();
+        await expect(this.homePage.featuredSection).toBeVisible();
+        await expect(this.homePage.categorySidebar).toBeVisible();
     }
-    
+
+    async verifyNavbarLinks() {
+        const links = [
+            'Home',
+            'Products',
+            'Cart',
+            'Signup / Login',
+            'Test Cases',
+            'API Testing',
+            'Video Tutorials',
+            'Contact us'
+        ];
+
+        for (let i = 0; i < links.length; i++) {
+            await expect(this.homePage.navLinks.nth(i)).toContainText(links[i]);
+        }
+    }
+
+    async verifyLogoRedirect() {
+        await this.page.goto('/products');
+        await this.homePage.clickLogo();
+        await expect(this.page).toHaveURL('/');
+    }
+
+    async verifyFeaturedProducts() {
+        await this.homePage.featuredSection.scrollIntoViewIfNeeded();
+        await expect(this.homePage.featuredSection).toBeVisible();
+
+        const count = await this.homePage.productCards.count();
+        expect(count).toBeGreaterThan(0);
+
+        for (let i = 0; i < Math.min(count, 3); i++) {
+            await expect(this.homePage.productImages.nth(i)).toBeVisible();
+            await expect(this.homePage.productNames.nth(i)).toBeVisible();
+            await expect(this.homePage.productPrices.nth(i)).toBeVisible();
+            await expect(this.homePage.addToCartButtons.nth(i)).toBeVisible();
+        }
+    }
+
+    async verifyCategorySidebar() {
+        await expect(this.homePage.categorySidebar).toBeVisible();
+        await expect(this.homePage.womenCategory).toBeVisible();
+        await expect(this.homePage.menCategory).toBeVisible();
+        await expect(this.homePage.kidsCategory).toBeVisible();
+    }
 }
--- src/page/homepage.ts
+++ src/page/homepage.ts
@@ -1,12 +1,40 @@
-import{Locator,Page} from "@playwright/test";
-export class HomePage{
-    readonly page:Page
-    readonly AutomationImage :Locator;
-    readonly  SignUp :Locator
-    constructor(page:Page){
-        this.page=page;
-        this.AutomationImage=page.getByAltText("Website for automation practice")
-        this.SignUp=page.locator("//a[text()=' Signup / Login']")
-        
+import { Page, Locator } from '@playwright/test';
+
+export class HomePage {
+
+    readonly page: Page;
+    readonly logo: Locator;
+    readonly navbar: Locator;
+    readonly navLinks: Locator;
+    readonly featuredSection: Locator;
+    readonly categorySidebar: Locator;
+    readonly productCards: Locator;
+    readonly productImages: Locator;
+    readonly productNames: Locator;
+    readonly productPrices: Locator;
+    readonly addToCartButtons: Locator;
+    readonly womenCategory: Locator;
+    readonly menCategory: Locator;
+    readonly kidsCategory: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.logo = page.locator('a[href="/"] img');
+        this.navbar = page.locator('ul.nav.navbar-nav');
+        this.navLinks = this.navbar.getByRole('link');
+        this.featuredSection = page.locator('.features_items');
+        this.categorySidebar = page.locator('.left-sidebar');
+        this.productCards = page.locator('.product-image-wrapper');
+        this.productImages = this.productCards.locator('img');
+        this.productNames = this.productCards.locator('.productinfo p');
+        this.productPrices = this.productCards.locator('.productinfo h2');
+        this.addToCartButtons = this.productCards.locator('a.add-to-cart:visible');
+        this.womenCategory = this.categorySidebar.getByText('Women', { exact: true }); 
+        this.menCategory = this.categorySidebar.getByText('Men', { exact: true }); 
+        this.kidsCategory = this.categorySidebar.getByText('Kids', { exact: true });
+    }
+
+    async clickLogo() {
+        await this.logo.click();
     }
 }
